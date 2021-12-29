3a35130e70074ab4348b1a8c373f422420cfaadd
James McArthur <James@zetane.com>
2021-12-28 19:16:52 -0800

    dope demo completet

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script>
 	import { fade, fly } from "svelte/transition";
+	import { randomStore } from "./store";
+	import Child from './Child.svelte'
+	//randomStore.set or update
+	//randomStore.update((v) => v + 1); //gives current value
+	//randomStore.subscribe(v => ...) need to manage subscriptions carefully
+	let data = {
+		userId: 123456,
+		name: 'james',
+		email: 'banana@bananac.om'
+	}
 	export let name;
 	let rando;
 	$: result = Math.round(rando * 100);
-
+	let randos = [];
 	function setRando() {
 		rando = Math.random();
+		randos = [...randos, rando]; //instead of .push(rando) as svlete is reactive to assignment
 	}
+
+	function delay(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+	let rando2 = delay(2000).then((v) => Math.random());
 </script>
 
 <main>
@@ -15,7 +31,8 @@
 		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
 		how to build Svelte apps.
 	</p>
-
+	<Child userId={data.userId} name={data.name} email={data.email}/>
+	<Child {...data}/>
 	<h1>Rando is {result}</h1>
 	<button on:click={setRando}>Randomize</button>
 	<input bind:value={rando} />
@@ -32,6 +49,24 @@
 	{:else}
 		<p>loser</p>
 	{/if}
+	<hr />
+	<!-- {$randomStore} subscribes to it -->
+	{$randomStore}
+	<button on:click={() => randomStore.set(Math.random())}>Siiiccckk</button>
+
+	{#await rando2}
+		<p>thinking about it...</p>
+	{:then number}
+		<p>number: {number}</p>
+	{:catch error}
+		<p>{error.message}</p>
+	{/await}
+
+	<ul>
+		{#each randos as val, idx}
+			<p>{val}</p>
+		{/each}
+	</ul>
 </main>
 
 <style>
